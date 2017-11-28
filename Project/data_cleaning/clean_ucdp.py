16b27643ed84fd406154d00225dd0d3a52d978d7
julied20 <julie@djeff.al>
2017-11-28 15:39:52 +0100

    Create .py clean UCDP

--- Project/data_cleaning/clean_ucdp.py
+++ Project/data_cleaning/clean_ucdp.py
@@ -20,7 +20,14 @@ def clean_ucdp():
 
         # If the column territory is empty, we can assume that the conflict happens at the same place than the variable 'location'. So we set the 'terr' variable with the 'location' value to avoid NaN. 
         if pd.isnull(row['terr']) :
-            conflict_df.loc[index, 'terr'] = row['location']   
+            conflict_df.loc[index, 'terr'] = row['location'] 
+        
+    #We have another csv that represent the countries, their ISO and their geographic coordinate. It will be useful to put display theses countries on a map. But some countries like South Vietnam, North Vietnam or South Yemen are not in this file, because they are not countries anymore. So the decision is made to rename these country with the actual name. 
+    for index, row in conflict_df.iterrows():
+        if conflict_df.loc[index, 'location'] == 'North Vietnam' or conflict_df.loc[index, 'location'] == 'South Vietnam, Vietnam' or conflict_df.loc[index, 'location'] == 'South Vietnam':
+            conflict_df.loc[index, 'location'] = 'Vietnam'
+        if conflict_df.loc[index, 'location'] == 'South Yemen':
+            conflict_df.loc[index, 'location'] = 'Yemen'    
     
     # We drop the useless columns
     conflict_df = conflict_df.drop(['gwnoa2nd', 'gwnob2nd', 'gwnoloc', 'gwnoa', 
@@ -39,22 +46,25 @@ def clean_ucdp():
     iso_conflict = []
     
     # The location of the conflict should be in the variable location. Sometimes, in this variable, a specific region of a country is named, which doesn't have an  ISO. In this case, the choice is made to set the ISO from the sidea country, as the sidea represent the primary party to a conflict
+    iso_conflict = []
+
     for i in range(0, len(sidea)):
-        if sidea[i] in iso_countries or location[i] in iso_countries:
-            for j in range(0, len(iso_countries)): 
-                if location[i] == iso_countries[j]:
-                    iso_conflict.append(iso_code[j])
-                    break
-                elif sidea[i] == iso_countries[j]:
-                    iso_conflict.append(iso_code[j])
-                    break
+        if terr[i] in iso_countries:
+            j = iso_countries.index(terr[i])
+            iso_conflict.append(iso_code[j])
+        elif location[i] in iso_countries:
+            j = iso_countries.index(location[i])
+            iso_conflict.append(iso_code[j])
+        elif sidea[i] in iso_countries:
+            j = iso_countries.index(sidea[i])
+            iso_conflict.append(iso_code[j])
         else:
             iso_conflict.append(0)
 
-    clean_filename = 'clean_conflict.csv'
+
+    clean_filename = 'datasets/clean_conflict.csv'
     conflict_df = conflict_df.assign(ISO2 = iso_conflict )
     conflict_df.to_csv(clean_filename)
-    os.rename(clean_filename,'../datasets/'+ clean_filename)
     
     return clean_filename
     
