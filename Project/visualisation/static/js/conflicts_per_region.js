c6e7c04e3caf173b2a5b70f8843e98a14f1ecba1
julied20 <julie@djeff.al>
2017-12-19 09:22:35 +0100

    Add graph coinflicts per regions

--- Project/visualisation/static/js/conflicts_per_region.js
+++ Project/visualisation/static/js/conflicts_per_region.js
@@ -1,132 +1,175 @@
 /*************************************/
 // Number of conflicts per years
 function get_conflicts_per_region(years, america, europe, africa, middle_east, asia) {
-  let labels = [];
-  let data = [];
+
+    new Chart(document.getElementById("conflicts_region"), {
+        type: 'bar',
+        data: {
+            labels: years,
+            datasets: [
+                {
+                    label: "Number of conflicts in Americas",
+                    data: america,
+                    backgroundColor: 'rgb(191, 63, 63)',
+                },
+                {
+                    label: "Number of conflicts in Europe",
+                    data: europe,
+                    backgroundColor: 'rgb(63, 191, 63)',
+                },
+                {
+                    label: "Number of conflicts in Africa",
+                    data: africa,
+                    backgroundColor: 'rgb(63, 63, 191)',
+                },
+                {
+                    label: "Number of conflicts in Middle East",
+                    data: middle_east,
+                    backgroundColor: 'rgb(191, 191, 63)',
+                },
+                {
+                    label: "Number of conflicts in Asia",
+                    data: asia,
+                    backgroundColor: 'rgb(191, 127, 63)',
+                }
+            ]
+        },
+        options: {
+            scales: {
+                xAxes: [{ stacked: true }],
+                yAxes: [{ stacked: true }]
+            },
+            legend: {
+                onClick: null
+            },
+            maintainAspectRatio: false
+        }
+    });
+
 
 
     new Chart(document.getElementById("conflicts_am"), {
         type: 'bar',
         data: {
-          labels: years,
-          datasets: [
-            {
-              label: "Number of conflicts",
-              backgroundColor: "rgba(255, 206, 86, 1)",
-              data: america
-            }
-          ]
+            labels: years,
+            datasets: [
+                {
+                    label: "Number of conflicts",
+                    backgroundColor: "rgba(255, 206, 86, 1)",
+                    data: america
+                }
+            ]
         },
         options: {
-          legend: { display: false },
-          title: {
-            display: true,
-            text: 'Conflicts in Americas'
-          },
-          legend: {
-            onClick: null
-          },
-          maintainAspectRatio: false
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Conflicts in Americas'
+            },
+            legend: {
+                onClick: null
+            },
+            maintainAspectRatio: false
         }
     });
 
     new Chart(document.getElementById("conflicts_eur"), {
         type: 'bar',
         data: {
-          labels: years,
-          datasets: [
-            {
-              label: "Number of conflicts",
-              backgroundColor: "rgba(255, 206, 86, 1)",
-              data: europe
-            }
-          ]
+            labels: years,
+            datasets: [
+                {
+                    label: "Number of conflicts",
+                    backgroundColor: "rgba(255, 206, 86, 1)",
+                    data: europe
+                }
+            ]
         },
         options: {
-          legend: { display: false },
-          title: {
-            display: true,
-            text: 'Conflicts in Europe'
-          },
-          legend: {
-            onClick: null
-          },
-          maintainAspectRatio: false
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Conflicts in Europe'
+            },
+            legend: {
+                onClick: null
+            },
+            maintainAspectRatio: false
         }
     });
 
     new Chart(document.getElementById("conflicts_afr"), {
         type: 'bar',
         data: {
-          labels: years,
-          datasets: [
-            {
-              label: "Number of conflicts",
-              backgroundColor: "rgba(255, 206, 86, 1)",
-              data: africa
-            }
-          ]
+            labels: years,
+            datasets: [
+                {
+                    label: "Number of conflicts",
+                    backgroundColor: "rgba(255, 206, 86, 1)",
+                    data: africa
+                }
+            ]
         },
         options: {
-          legend: { display: false },
-          title: {
-            display: true,
-            text: 'Conflicts in Africa'
-          },
-          legend: {
-            onClick: null
-          },
-          maintainAspectRatio: false
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Conflicts in Africa'
+            },
+            legend: {
+                onClick: null
+            },
+            maintainAspectRatio: false
         }
     });
 
     new Chart(document.getElementById("conflicts_midle"), {
         type: 'bar',
         data: {
-          labels: years,
-          datasets: [
-            {
-              label: "Number of conflicts",
-              backgroundColor: "rgba(255, 206, 86, 1)",
-              data: middle_east
-            }
-          ]
+            labels: years,
+            datasets: [
+                {
+                    label: "Number of conflicts",
+                    backgroundColor: "rgba(255, 206, 86, 1)",
+                    data: middle_east
+                }
+            ]
         },
         options: {
-          legend: { display: false },
-          title: {
-            display: true,
-            text: 'Conflicts in Middle East'
-          },
-          legend: {
-            onClick: null
-          },
-          maintainAspectRatio: false
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Conflicts in Middle East'
+            },
+            legend: {
+                onClick: null
+            },
+            maintainAspectRatio: false
         }
     });
 
     new Chart(document.getElementById("conflicts_asia"), {
         type: 'bar',
         data: {
-          labels: years,
-          datasets: [
-            {
-              label: "Number of conflicts",
-              backgroundColor: "rgba(255, 206, 86, 1)",
-              data: asia
-            }
-          ]
+            labels: years,
+            datasets: [
+                {
+                    label: "Number of conflicts",
+                    backgroundColor: "rgba(255, 206, 86, 1)",
+                    data: asia
+                }
+            ]
         },
         options: {
-          legend: { display: false },
-          title: {
-            display: true,
-            text: 'Conflicts in Asia'
-          },
-          legend: {
-            onClick: null
-          },
-          maintainAspectRatio: false
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Conflicts in Asia'
+            },
+            legend: {
+                onClick: null
+            },
+            maintainAspectRatio: false
         }
     });
 
